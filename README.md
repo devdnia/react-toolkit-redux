498a3ea660858fa397017ed029516f4473aa8c58
Ivan <devdnia@gmail.com>
2022-06-21 11:30:27 +0200

    Subiendo APP

---
+++ README.md
@@ -0,0 +1,11 @@
+## Redux, redux-toolkit y RTK query  
+
+### Instalación  
+Instalar las dependencias de    Node
+~~~
+yarn
+~~~
+
+### Descripción
+Esta app de REACT muestra como utilizar el store, slices, thunks, RTK-query. Las peticiones http se han realizado con la libreria axios.  
+
---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,27 @@
+import { pokemonApi } from '../../../api/pokemonApi';
+import { setPokemons, startLoadingPokemons } from './pokemonSlice';
+
+export const getPokemons = ( page = 0 ) =>{
+
+    return async( dispatch, getState ) => {
+        dispatch( startLoadingPokemons() );
+
+        // fetch
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10}`);
+        // const data = await resp.json();
+        
+        // dispatch( setPokemons( {
+        //     pokemons: data.results,
+        //     page: page + 1
+        //}));
+
+        // axios
+        // pokemonApi.js
+        const {data} = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10}`);
+
+        dispatch( setPokemons( {
+            pokemons: data.results,
+            page: page + 1
+         }));
+    }
+}
